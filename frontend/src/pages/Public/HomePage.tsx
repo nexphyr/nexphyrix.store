43f7c3792fc91d09158d530e992d0c4da6e94c7e
unknown <zephyrus8123@gmail.com>
2026-09-02 19:31:52 +0700

    Remove FOMO notification feature

--- frontend/src/pages/Public/HomePage.tsx
+++ frontend/src/pages/Public/HomePage.tsx
@@ -8,7 +8,6 @@ import CheckoutModal from '../../components/Cart/CheckoutModal';
 import ToastContainer from '../../components/Cart/ToastContainer';
 import AuthMenu from '../../components/AuthMenu';
 import PromoCarousel from '../../components/PromoCarousel';
-import FomoNotification from '../../components/FomoNotification';
 import { useCart } from '../../contexts/CartContext';
 import logoImage from '../../assets/nexphyrix.png';
 
@@ -333,7 +332,6 @@ const HomePage = () => {
       </footer>
 
       {/* Cart Components */}
-      <FomoNotification />
       <FloatingCart />
       <CartDrawer />
       <CheckoutModal />
